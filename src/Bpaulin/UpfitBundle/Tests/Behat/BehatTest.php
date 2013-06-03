2d73d1a507c2e98fb99b0a603e2d01cdc8135c4c
Bruno Paulin <brunopaulin@bpaulin.net>
2013-06-03 15:58:46 +0200

    [build] coverage & parameters

---
+++ src/Bpaulin/UpfitBundle/Tests/Behat/BehatTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bpaulin\UpfitBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Behat\Behat\Console\BehatApplication;
+
+class BehatTest extends WebTestCase
+{
+    /**
+     * @group behat
+     */
+    public function testBehat()
+    {
+        try {
+            $input = new ArrayInput(array('--profile' => 'report', '--ansi' =>''));
+            $output = new ConsoleOutput();
+
+            $app = new \Behat\Behat\Console\BehatApplication('dev');
+            $app->setAutoExit(false);
+
+            $result = $app->run($input, $output);
+
+            $this->assertEquals(0, $result);
+        } catch (\Exception $exception) {
+            $this->fail($exception->getMessage());
+        }
+    }
+}
