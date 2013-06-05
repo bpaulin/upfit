866003a11f09d3ecb802d30812dcee765854d821
Bruno Paulin <brunopaulin@bpaulin.net>
2013-06-05 03:48:38 +0200

    stage form

---
+++ src/Bpaulin/UpfitBundle/Form/StageType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bpaulin\UpfitBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class StageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('order')
+            ->add('exercise')
+            ->add('program')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Bpaulin\UpfitBundle\Entity\Stage'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'bpaulin_upfitbundle_stagetype';
+    }
+}
