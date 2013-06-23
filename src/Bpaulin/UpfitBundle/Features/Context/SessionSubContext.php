33c5b7bc87cadeffd92563a15fddfe8cd6edcd70
Bruno Paulin <brunopaulin@bpaulin.net>
2013-06-23 19:06:41 +0200

    failing: Member can begin a session from program list

---
+++ src/Bpaulin/UpfitBundle/Features/Context/SessionSubContext.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Bpaulin\UpfitBundle\Features\Context;
+
+use Behat\Behat\Context\BehatContext;
+use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Context\Step;
+use Behat\Behat\Exception\PendingException;
+
+class SessionSubContext extends BehatContext
+{
+    public function __construct()
+    {
+        // do subcontext initialization
+    }
+
+    /**
+     * @Then /^I should see a link to begin session following "([^"]*)" "([^"]*)"$/
+     */
+    public function iShouldSeeALinkToBeginSessionFollowing($type, $name)
+    {
+        if ($type != 'program') {
+            throw new \Exception('only available for program');
+        }
+        $em = $this->getMainContext()->getKernel()->getContainer()->get('doctrine')->getManager();
+        $program = $em->getRepository('BpaulinUpfitBundle:Program')->findOneByName($name);
+        if (!$program) {
+            throw new \Exception('program not found');
+        }
+        return new Step\Then("I should see a link to \"/member/session/new/program/".$program->getId()."\"");
+    }
+
+    /**
+     * @Then /^I should be on "([^"]*)" workout page$/
+     */
+    public function iShouldBeOnWorkoutPage($name)
+    {
+        $em = $this->getMainContext()->getKernel()->getContainer()->get('doctrine')->getManager();
+        $exercise = $em->getRepository('BpaulinUpfitBundle:Exercise')->findOneByName($name);
+        if (!$exercise) {
+            throw new \Exception('exercise not found');
+        }
+        return array(
+            new Step\Then("the \"#exercise-name\" element should contain \"".$exercise->getName()."\""),
+        );
+    }
+
+    /**
+     * @Given /^I fill in session form with the following:$/
+     */
+    public function iFillInSessionFormWithTheFollowing(TableNode $table)
+    {
+        //bpaulin_upfitbundle_programtype_stages_0_difficultyUnit
+        $form = 'bpaulin_upfitbundle_sessiontype_';
+        foreach ($table->getRowsHash() as $field => $value) {
+            $this->getMainContext()->fillField($form.$field, $value);
+        }
+    }
+
+    /**
+     * @Given /^I should see a link to session "([^"]*)"$/
+     */
+    public function iShouldSeeALinkToSession($name)
+    {
+        $em = $this->getMainContext()->getKernel()->getContainer()->get('doctrine')->getManager();
+        $session = $em->getRepository('BpaulinUpfitBundle:Session')->findOneByName($name);
+        if (!$session) {
+            return true;
+        }
+        return new Step\Then("I should see a link to \"/member/session/".$session->getId()."\"");
+    }
+}
---
+++ src/Bpaulin/UpfitBundle/Features/session.feature
@@ -0,0 +1,59 @@
+@wip
+Feature: session management
+  In order to practice
+  As an member
+  I need to follow exercises
+
+Background:
+  Given I am member
+
+Scenario: Member can begin a session from program list
+  Given I am on "/member/program"
+  Then I should see a link to begin session following "program" "program1"
+  When I follow this link
+  Then I should be on "exercise1" workout page
+  When I follow "Abandon"
+  Then I should see a "info" message "exercise1 abandonned"
+  And I should be on "exercise2" workout page
+  When I follow "Pass"
+  Then I should see a "info" message "exercise2 passed"
+  And I should be on "exercise3" workout page
+  When I follow "Done"
+  Then I should be on "exercise4" workout page
+  When I follow "Done"
+  Then I should be on "exercise5" workout page
+  When I follow "Done"
+  Then I should be on "exercise2" workout page
+  When I follow "Done"
+  # Then I should be on "/member/session/edit"
+  And I should see a "success" message "session finished"
+    And I fill in session form with the following:
+    | name       | session1   |
+    | comment    | comment1   |
+    | difficulty | 1          |
+  And I press "Edit"
+  Then I should be on "/member/session"
+  And I should see a "success" message "session1 updated"
+  And I should see a link to session "session1"
+
+# Scenario: Member can begin a session from session list
+#   Given I am on "/member/session"
+#   Then I should see a link to begin session following "session" "session1"
+#   When I follow this link
+#   Then I should be on "exercise3" workout page
+#   When I follow "Done"
+#   Then I should be on "exercise4" workout page
+#   When I follow "Done"
+#   Then I should be on "exercise5" workout page
+#   When I follow "Done"
+#   Then I should be on "exercise2" workout page
+#   When I follow "Done"
+#   Then I should be on "/member/session/save"
+#   And I should see a "success" message "session finished"
+#     And I fill in session form with the following:
+#     | name       | session1   |
+#     | comment    | comment1   |
+#     | difficulty | 1          |
+#   And I press "Save"
+#   Then I should be on "/member/session"
+#   Then I should see a "success" message "session finished"
