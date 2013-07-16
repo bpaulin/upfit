e7e5a79fb83b2a2f57bf713a39a0395353d16c1a
Bruno Paulin <brunopaulin@bpaulin.net>
2013-07-16 12:57:19 +0200

    improve build
    separate behat tests with and without browser
    Report behat coverage for controller and features

--- src/Bpaulin/UpfitBundle/Tests/Behat/BehatTest.php
+++ src/Bpaulin/UpfitBundle/Tests/Behat/BehatTest.php
@@ -11,6 +11,28 @@ class BehatTest extends WebTestCase
 {
     /**
      * @group behat
+     * @group behat-without-browser
+     */
+    public function testBehatWithoutBrowser()
+    {
+        try {
+            $input = new ArrayInput(array('--profile' => 'report', '--ansi' =>'', '--tags' => "~@javascript"));
+            $output = new ConsoleOutput();
+
+            $app = new \Behat\Behat\Console\BehatApplication('dev');
+            $app->setAutoExit(false);
+
+            $result = $app->run($input, $output);
+
+            $this->assertEquals(0, $result);
+        } catch (\Exception $exception) {
+            $this->fail($exception->getMessage());
+        }
+    }
+
+    /**
+     * @group behat
+     * @group behat-with-browser
      */
     public function testBehat()
     {
