b8f07db33883d3c74b659121788efa813c3b3cc2
Bruno Paulin <brunopaulin@bpaulin.net>
2013-07-19 19:58:38 +0200

    config capifony

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,19 +1,36 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
-set :app_path,    "app"
+# set :domain, ""
+# set :user, ""
+# set :webserver_user, "
+# set :application,   ""
+# set :branch,        ""
+# set :deploy_to,     ""
+# set :keep_releases, 5
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
-set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+set :stages,        %w(master develop)
+set :default_stage, "develop"
+set :stage_dir,     "app/config"
+require 'capistrano/ext/multistage'
 
-set :model_manager, "doctrine"
-# Or: `propel`
+set   :app_path,      "app"
+set   :repository,       "https://github.com/bpaulin/upfit.git"
+set   :deploy_via,       :capifony_copy_local
+set   :use_composer,     true
+set   :use_composer_tmp, true
+set   :scm,              :git
+set   :model_manager,    "doctrine"
+role  :db,              domain, :primary => true
+role  :web,              domain
+role  :app,             domain, :primary => true
+set   :use_sudo,      false
+set   :shared_files,      ["app/config/parameters.yml"]
+set   :writable_dirs,       ["app/cache", "app/logs"]
+set   :permission_method,   :acl
+set   :use_set_permissions, true
 
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain, :primary => true       # This may be the same as your `Web` server
+default_run_options[:pty] = true
 
-set  :keep_releases,  3
+logger.level = Logger::INFO
 
-# Be more verbose by uncommenting the following line
-# logger.level = Logger::MAX_LEVEL
+set :default_environment, {
+  :LANG => 'en_US.UTF-8'
+}
