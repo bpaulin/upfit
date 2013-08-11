89abe5a2d3df2786a280ea4c5baec04f36b5e676
Bruno Paulin <brunopaulin@bpaulin.net>
2013-08-11 15:48:57 +0200

    pass check:phpcpd

---
+++ src/Bpaulin/UpfitBundle/Controller/AbstractController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Bpaulin\UpfitBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Bpaulin\UpfitBundle\Entity\Program;
+use Bpaulin\UpfitBundle\Form\ProgramType;
+
+/**
+ * Abstract controller.
+ */
+abstract class AbstractController extends Controller
+{
+    /**
+     * Creates a form to delete an entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    protected function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm();
+    }
+
+    /**
+     * Deletes an entity.
+     */
+    public function deleteAbstractAction($entity)
+    {
+        $deleteForm = $this->createDeleteForm($entity->getId());
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+}
---
+++ src/Bpaulin/UpfitBundle/Entity/MuscleSettings.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Bpaulin\UpfitBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * ExerciseSettings
+ */
+class MuscleSettings
+{
+
+    /**
+     * muscle
+     *
+     * @ORM\ManyToOne(targetEntity="Muscle")
+     */
+    protected $muscle;
+
+    /**
+     * Set muscle
+     *
+     * @param  \Bpaulin\UpfitBundle\Entity\Muscle $muscle
+     * @return Objective
+     */
+    public function setMuscle(\Bpaulin\UpfitBundle\Entity\Muscle $muscle = null)
+    {
+        $this->muscle = $muscle;
+
+        return $this;
+    }
+
+    /**
+     * Get muscle
+     *
+     * @return \Bpaulin\UpfitBundle\Entity\Muscle
+     */
+    public function getMuscle()
+    {
+        return $this->muscle;
+    }
+}
